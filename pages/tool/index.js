02daaffdcbe92798b95f7ea9e5fd4b0f8a6aa095
lihao <15502354225@163.com>
2022-08-30 18:20:29 +0800

    第三版

---
+++ pages/tool/index.js
@@ -0,0 +1,38 @@
+// pages/tool/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        dataList: [
+            {
+                background: 'linear-gradient(90deg, #8469FF, #1c698e)',
+                title: '渝康码',
+                icon: '../../img/app-logo/yukangma.png',
+            },
+            {
+                background: 'linear-gradient(90deg,  #1c698e, #1c8e83)',
+                title: '京东',
+                icon: '../../img/app-logo/yukangma.png',
+            },
+            {
+                background: 'linear-gradient(90deg,  #1c8e83, #197d46)',
+                title: '拼多多',
+                icon: '../../img/app-logo/yukangma.png',
+            },
+            {
+                background: 'linear-gradient(90deg,  #197d46, #60ab0d)',
+                title: '淘宝',
+                icon: '../../img/app-logo/yukangma.png',
+            }
+        ]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+})
